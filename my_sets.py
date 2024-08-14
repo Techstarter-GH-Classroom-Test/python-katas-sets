9184b1bb562ba331c2a8f2a858e1d4ef74bb320b
Niklas Oetken <niklas.oetken@techstarter.de>
2024-08-14 10:47:33 +0200

    push tests initial commit

--- my_sets.py
+++ my_sets.py
@@ -3,3 +3,43 @@ it_companies = {'Meta', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
 age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+# Tausche die ___ mit dem korrekten Code:
+
+# Aufgabe 1:  Gib die Länge der Menge `it_companies`aus.
+# length = ___
+
+
+# Aufgabe 2: Füge die Firma `OpenAI` zu der Menge `it_companies` hinzu.
+# ___
+
+# Aufgabe 3: Lösche die Firma `Meta`aus der Menge `it_companies`.
+# ___
+
+# Aufgabe 4: Finde den Durschnitt der Mengen `A` und `B`.
+# ___
+
+# Aufgabe 5: Finde den Schnittpunkt der Mengen `A`und `B`.
+# ___
+
+# Aufgabe 6: Finde heraus ob `A` eine Teilmenge von `B` ist.
+# ___
+
+# Aufgabe 7: Finde die symmetrische Differenz der Mengen `A` und `B`.
+# ___
+
+# Aufgabe 8: Lösche `A`und `B`.
+# ___
+
+# Aufgabe 9: Konvertiere die Liste `ages`zu einer Menge.
+# ___
+
+# Aufgabe 10: Wie viele einzigartige Worte befinden sich in folgendem Satz: 
+# "Ich liebe es zu coden und ich kann es kaum erwarten mehr zu lernen". 
+# Schreibe eine Funktion, die diesen Satz als Parameter aufnimmt 
+# und die Anzahl an einzigartigen (unique) Worten ausgibt.
+# ___
+
+# Nachdem Sie alle Übungen abgeschlossen haben, speichern Sie diese Datei und führen Sie sie aus, 
+# um sicherzustellen, dass keine Fehler vorliegen.
+# Die Übungen sind abgeschlossen, wenn Sie diese Datei erfolgreich und ohne Fehler ausgeführt haben.
